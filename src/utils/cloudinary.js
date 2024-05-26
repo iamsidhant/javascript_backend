2c280718e9b97578cb069d14ee1fb7339fc6f642
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-05-26 12:44:51 +0530

    add user register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        // console.log("file is uploaded om cloudinary ", response.url);
+        console.log("file is uploaded om cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
         
