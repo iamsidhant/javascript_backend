e280d4bb464742362d76ba4b3324e308c29da87e
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-05-27 11:43:31 +0530

    bug fixing of user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,17 +16,17 @@ const registerUser = asyncHandler( async(req, res) => {
     // check for user creation
     // return res
 
-    const {fullName, email, userName, password } = req.body
-    console.log("email: ", email);
+    const {fullName, email, username, password } = req.body
+    // console.log("email: ", email);
 
     if (
-        [fullName, email, userName, password].some((field) => field?.trim() === "")
+        [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
     }
 
     const existedUser = await User.findOne({
-        $or: [{ userName }, { email }]
+        $or: [{ username }, { email }]
     })
 
     if (existedUser) {
@@ -35,7 +35,13 @@ const registerUser = asyncHandler( async(req, res) => {
     // console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -48,13 +54,14 @@ const registerUser = asyncHandler( async(req, res) => {
         throw new ApiError(400, "Avatar file is required")
     }
 
+
     const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
         password,
-        userName: userName.toLowerCase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
